1ca30112a0bf1141e6a08b5de5591a570578e0a5
SHELDON SMALL <S.SMALL-2021@hull.ac.uk>
2023-03-30 10:51:17 +0100

    Started Capstone Project

--- Capstone-Project-441101-2223/Capstone-Project-441101-2223/Program.cs
+++ Capstone-Project-441101-2223/Capstone-Project-441101-2223/Program.cs
@@ -1,2 +1,20 @@
 ﻿// See https://aka.ms/new-console-template for more information
+using Capstone_Project_441101_2223;
+
 Console.WriteLine("Hello, World!");
+
+ProjectManager projectManager = new ProjectManager();
+
+LandProject greenland = new LandProject(5000);
+
+RenovationProject house = new RenovationProject(10000);
+
+projectManager.AddProject(greenland);
+
+projectManager.AddProject(house);
+
+for (int i = 0; i < projectManager.Projects.Count; i++)
+{
+    Console.WriteLine(projectManager.Projects.ToString);
+}
+
---
+++ Capstone-Project-441101-2223/Capstone-Project-441101-2223/Projects.cs
@@ -0,0 +1,126 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Capstone_Project_441101_2223
+{
+    internal class ProjectManager
+    {
+        public List<Project> Projects { get; private set; }
+
+        public ProjectManager()
+        {
+            Projects = new List<Project>();
+        }
+
+        public void AddProject(Project pProject)
+        {
+            Projects.Add(pProject);
+        }
+
+    }
+    public abstract class Project
+    {
+
+        public int ID { get; private set; } // ID for all projects 
+
+        static int NextID;
+
+        public Project()
+        {
+            ID = NextID;
+            NextID++;
+        }
+
+
+    }
+
+    public class LandProject : Project
+    {
+
+        private float Purchases;
+        private float Sales;
+        private float Refunds;
+        private float Profits;
+
+        public float purchases
+        {
+            get { return Purchases; }private set { }
+
+        }
+
+        public float sales
+        {
+            get { return Sales; }private set { }
+
+        }
+
+        public float refunds
+        {
+            get { return Refunds; }private set { }
+
+        }
+
+        public float profits
+        {
+            get { return Profits; }private set { }
+
+        }
+
+        public LandProject(float pCostOfLand)
+        {
+            purchases = pCostOfLand;
+        }
+
+        public override string ToString()
+        {
+            return $"Purchases: {Purchases} \n Sales:{Sales} \n Refunds:{Refunds} \n Profits:{Profits}.";
+        }
+
+    }
+
+    public class RenovationProject : Project
+    {
+
+        private float Purchases;
+        private float Sales;
+        private float Refunds;
+        private float Profits;
+
+        public float purchases
+        {
+            get { return Purchases; }private set { }
+
+ }
+        public float sales
+        {
+            get { return Sales; }private set { }
+
+        }
+        public float refunds
+        {
+            get { return Refunds; }private set { }
+
+        }
+
+        public float profits
+        {
+            get { return Profits; } private set { }
+
+        }
+
+        public RenovationProject(float pCostOfProperty)
+        {
+            purchases = pCostOfProperty;
+        }
+
+        public override string ToString()
+        {
+            return $"Purchases: {Purchases} \n Sales:{Sales} \n Refunds:{Refunds} \n Profits:{Profits}.";
+        }
+    }
+}
